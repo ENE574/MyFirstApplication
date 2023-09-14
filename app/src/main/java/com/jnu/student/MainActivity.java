7df2b27a3d43d0e2d2297d6b4d986c9e137d621c
574lrg <2825119706@qq.com>
2023-09-14 11:21:16 +0800

    view

--- app/src/main/java/com/jnu/student/MainActivity.java
+++ app/src/main/java/com/jnu/student/MainActivity.java
@@ -2,7 +2,10 @@ package com.jnu.student;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+//        setContentView(R.layout.activity_main);
+//        RelativeLayout relativeLayout = new RelativeLayout(this);
+//        RelativeLayout.LayoutParams params =  new RelativeLayout.LayoutParams(
+//                RelativeLayout.LayoutParams.WRAP_CONTENT,
+//                RelativeLayout.LayoutParams.WRAP_CONTENT);
+//        params.addRule(RelativeLayout.CENTER_IN_PARENT);  //设置布局中的控件居中显示
+//        TextView textView = new TextView(this);                       //创建TextView控件
+//        textView.setText("你好Android");                     //设置TextView的文字内容
+//        textView.setTextColor(Color.RED);                                  //设置TextView的文字颜色
+//        textView.setTextSize(18);                                                //设置TextView的文字大小
+//        relativeLayout.addView(textView, params);                  //添加TextView对象和TextView的布局属性
+//        setContentView(relativeLayout);                                  //设置在Activity中显示RelativeLayout
+        TextView textView = findViewById(R.id.text_view_hello_world);
+        String helloText = getResources().getString(R.string.hello_android);
+        textView.setText(helloText);
     }
 }
