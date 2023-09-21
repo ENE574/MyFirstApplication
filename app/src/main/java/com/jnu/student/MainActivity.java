12293aea68d903d600830b8a9f0c61b64ffe07fb
574lrg <2825119706@qq.com>
2023-09-21 10:40:19 +0800

    exchange

--- app/src/main/java/com/jnu/student/MainActivity.java
+++ app/src/main/java/com/jnu/student/MainActivity.java
@@ -1,13 +1,20 @@
 package com.jnu.student;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView textViewHello;
+    private TextView textViewJNU;
+    private Button buttonChangeText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,5 +35,45 @@ public class MainActivity extends AppCompatActivity {
         TextView textView = findViewById(R.id.text_view_hello_world);
         String helloText = getResources().getString(R.string.hello_android);
         textView.setText(helloText);
+        setContentView(R.layout.activity_main);
+
+        textViewHello = findViewById(R.id.textViewHello);
+        textViewJNU = findViewById(R.id.textViewJNU);
+        buttonChangeText = findViewById(R.id.buttonChangeText);
+
+        buttonChangeText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String tempText = textViewHello.getText().toString();
+                textViewHello.setText(textViewJNU.getText());
+                textViewJNU.setText(tempText);
+
+                Toast.makeText(MainActivity.this, "交换成功", Toast.LENGTH_SHORT).show();
+
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                builder.setTitle("交换成功")
+                        .setMessage("两个TextView的文本已成功交换")
+                        .setPositiveButton("确定", null)
+                        .create()
+                        .show();
+            }
+        });
+        buttonChangeText.setOnClickListener(v -> {
+            // 交换两个TextView的文本
+            String temp = textViewHello.getText().toString();
+            textViewHello.setText(textViewJNU.getText().toString());
+            textViewJNU.setText(temp);
+
+            // 显示Toast
+            Toast.makeText(MainActivity.this, "交换成功", Toast.LENGTH_SHORT).show();
+
+            // 显示AlertDialog
+            new AlertDialog.Builder(MainActivity.this)
+                    .setTitle("成功")
+                    .setMessage("交换成功")
+                    .setPositiveButton("确定", null)
+                    .show();
+        });
     }
+
 }
